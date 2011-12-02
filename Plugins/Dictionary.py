924462d24ca742bce3059621b51c49b96588ef13
Ferus <mathewhudson93@gmail.com>
2011-12-01 18:41:01 -0800

    Boldfix v2.0

--- Plugins/Dictionary.py
+++ Plugins/Dictionary.py
@@ -24,19 +24,18 @@ class wordnikObj(object):
 			defs = []
 			deflist = self.Word.word_get_definitions(w)[0:3]
 			numdefs = len(deflist)
-
 			for x in deflist:
 				defs.append(x['text'])
 					
 			if numdefs == 0:
-				sock.send("PRIVMSG {0} :\x01[WordNik]\x01 I didn't find any definitions for '{1}'.".format(msg[3], w))
+				sock.send("PRIVMSG {0} :\x02[WordNik]\x02 I didn't find any definitions for '{1}'.".format(msg[3], w))
 			elif numdefs == 1:
-				sock.send("PRIVMSG {0} :\x01[WordNik]\x01 I found one definition for '{0}'.".format(msg[3], w))
-				sock.send("PRIVMSG {0} :\x01[WordNik]\x01 {1}: {2}".format(msg[3], w, defs[0]))
+				sock.send("PRIVMSG {0} :\x02[WordNik]\x02 I found one definition for '{1}'.".format(msg[3], w))
+				sock.send("PRIVMSG {0} :\x02[WordNik]\x02 {1}: {2}".format(msg[3], w, defs[0]))
 			else:
-				sock.send("PRIVMSG {0} :\x01[WordNik]\x01 I found {1} definitions for '{2}'.".format(msg[3], numdefs, w))
+				sock.send("PRIVMSG {0} :\x02[WordNik]\x02 I found {1} definitions for '{2}'.".format(msg[3], numdefs, w))
 				while len(defs) != 0:
-					sock.send("PRIVMSG {0} :\x01[WordNik]\x01 {1}: {2}".format(msg[3], w, defs.pop(0)))
+					sock.send("PRIVMSG {0} :\x02[WordNik]\x02 {1}: {2}".format(msg[3], w, defs.pop(0)))
 		else:
 			sock.say(msg[3], "Please install the python wordnik API and get a developers API key. http://developer.wordnik.com/libraries#python")
 
