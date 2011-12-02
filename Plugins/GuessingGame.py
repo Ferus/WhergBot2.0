4c82dd2682a397c9920350d83b87dde1f76d69d6
Ferus <mathewhudson93@gmail.com>
2011-12-02 02:14:52 -0800

    Stats?

--- Plugins/GuessingGame.py
+++ Plugins/GuessingGame.py
@@ -2,11 +2,107 @@
 
 from random import randint
 
+class GuessingStats(object):
+	'''
+	Stats recording for Guessing Game.
+	Stats are held in dictionaries.
+	Dict['User']['stat'] where stat is the stat you want.
+
+	List of stats per 'player':
+	notValid = Number of invalid numbers guessed
+	overMax = Number of guesses over the max number
+	underMin = Number of guesses under the min (One)
+	tooHigh = Number of guesses that were too high
+	tooLow = Number of guesses that were too low
+	wins = Number of correct guesses
+
+	List of global stats:
+	totalWins = Number of total wins across all games.
+	totalPlayers = Number of everyone who has ever played, by nick.
+		#len(db['Players'])
+	#The most people who have played at once
+	A function that gets absolute totals of all players' stats.
+	
+	{
+	'Players': {
+		'Ferus': {
+			'notValid':0,
+			'overMax':0, },
+
+		'ExamplePerson': {
+			'notValid':0,
+			'overMax':0, },
+		#End Players Block },
+	
+	#Globals
+	'regens':0,
+	'totalWins':0,
+	'totalPlayers':len(db['Players'])
+	}
+	#Etc
+	'''
+	
+	def __init__(self, _StatsDB=None):
+		import shelve
+		
+		if _StatsDB:
+			self._StatsDB = _StatsDB
+		else:
+			self._StatsDB = "Plugins/GuessingGameStats.shelve"
+
+		_db = shelve.open(self._StatsDB)
+		self.StatsDB = self.Load(_db)
+		_db.close()
+
+		if 'Players' not in self.StatsDB.keys():
+			self.StatsDB['Players'] = {}
+
+	def Load(self, _db):
+		x = {}
+		for key in _db.keys():
+			x[key] = _db[key]
+		return x
+		
+	def Save(self):
+		try:
+			_db = shelve.open(self.StatsDB)
+			for key in self.StatsDB.keys():
+				_db[key] = self.StatsDB[key]
+			_db.close()
+			return True
+		except:
+			return False
+			
+	def AddStats(self, Player=None, Stat=None, Value=None):
+		'''
+		Check if we are adding stats to a player.
+		If we are not, we fall back to globals.
+		'''
+		if Player:
+			if Player not in self.StatsDB['Players'].keys():
+				self.StatsDB['Players'][Player] = {}
+				
+			if Stat not in self.StatsDB['Players'][Player].keys():
+				self.StatsDB['Players'][Player][Stat] = Value
+			else:
+				self.StatsDB['Players'][Player][Stat] += Value
+
+		else:
+			if not self.StatsDB[Stat]:
+				self.StatsDB[Stat] = Value
+			else:
+				self.StatsDB[Stat] += Value
+			
+		
 class GuessingGame(object):
-	def __init__(self):
+	def __init__(self, _stats=False):
 		self.MaxNum = 250
 		self.CurrentNumber = None
 		self.GenNumber()
+
+		self._stats = _stats
+		if self._stats:
+			self.Stats = GuessingStats()
 
 	def GenNumber(self, sock=None, location=None):
 		self.CurrentNumber = randint(1, self.MaxNum)
@@ -26,30 +122,46 @@ class GuessingGame(object):
 			UserGuess = int(msg[4].split()[1])
 		except:
 			sock.say(msg[3], "I'm sorry {0}, but {1} doesn't seem to be a valid number.".format(msg[0], msg[4].split()[1]))
+			if self._stats:
+				self.Stats.AddStats(Player=msg[0], Stat='notValid', Value=1)
 			return None
 
 		if UserGuess > self.MaxNum:
 			sock.say(msg[3], "Sorry {0}, but my maximum number is set to {1}.".format(msg[0], self.MaxNum))
-			
+			if self._stats:
+				self.Stats.AddStats(Player=msg[0], Stat='overMax', Value=1)
+				
 		elif UserGuess <= 0:
 			sock.say(msg[3], "Sorry {0}, but my number cannot be lower than 0.".format(msg[0]))
-
+			if self._stats:
+				self.Stats.AddStats(Player=msg[0], Stat='underMin', Value=1)
+				
 		elif UserGuess > self.CurrentNumber:
 			sock.say(msg[3], "I'm sorry {0}, but my number is lower than that.".format(msg[0]))
-
+			if self._stats:
+				self.Stats.AddStats(Player=msg[0], Stat='tooHigh', Value=1)
+				
 		elif UserGuess < self.CurrentNumber:
 			sock.say(msg[3], "I'm sorry {0}, but my number is higher than that.".format(msg[0]))
-
+			if self._stats:
+				self.Stats.AddStats(Player=msg[0], Stat='tooLow', Value=1)
+				
 		elif UserGuess == self.CurrentNumber:
 			sock.say(msg[3], "Congratulations {0}, You guessed my number!".format(msg[0]))
 			self.GenNumber(sock=sock, location=msg[3])
-
+			if self._stats:
+				self.Stats.AddStats(Player=msg[0], Stat='wins', Value=1)
 		else:
 			pass #Wut.
+
+	def StatTest(self, msg, sock, users, allowed):
+		if self._stats:
+			sock.say(msg[3], str(self.Stats.StatsDB['Players'][msg[0]]))
 			
-G = GuessingGame()
+G = GuessingGame(_stats=True)
 
 hooks = {
 	'^@guess': [G.CheckGuess, 5, False],
 	'^@setguess': [G.SetMaxNum, 3, False],
+	'^@checkstats': [G.StatTest, 5, False],
 	}
