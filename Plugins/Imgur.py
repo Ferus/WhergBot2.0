bc9cffe6729d10895d7273e312635e2fc09c33dd
Ferus <mathewhudson93@gmail.com>
2012-02-06 15:53:40 -0800

    better..

--- Plugins/Imgur.py
+++ Plugins/Imgur.py
@@ -39,7 +39,7 @@ class Imgur(object):
 			stats['bandwidth'] = bw.split("</span>")[0].split("\"stat\">")[1]
 
 		return stats
-	
+
 	def Main(self, ID):
 		'''Fetch HTML, Returns a dict from the Parser'''
 		link = "http://imgur.com/gallery/{0}".format(ID)
@@ -48,30 +48,36 @@ class Imgur(object):
 			return "Couldn't connect to Imgur"
 		return self.Parser(html.content, link)
 
-I = Imgur()		
-		
+I = Imgur()
+
 def ImgurStats(msg, sock, users, allowed):
-	Links = [x for x in re.findall('(?:https?:\/\/)?(?:www\.)?(?:i\.)?imgur\.com\/(?:gallery\/)?[a-zA-Z0-9]{5}(?:\.)?(?:jpg|jpeg|png|gif)?', msg[4])]
+
+	y = msg[4].replace('gallery/', '')
+	Links = [x for x in re.findall('(?:https?:\/\/)?(?:www\.)?(?:i\.)?imgur\.com\/[a-zA-Z0-9]{5}(?:\.)?(?:jpg|jpeg|png|gif)?', y)]
 	ImageIds = []
 	for Link in Links:
-		[ImageIds.append(x) for x in re.findall("[a-zA-Z0-9]{5}", Link) if (x not in ImageIds and x != "imgur")]
+		[ImageIds.append(x) for x in re.findall("[a-zA-Z0-9]{5}", re.sub("(?:https:\/\/)?(:?www\.)?(?:i\.)?imgur\.com\/", "", Link)) if (x not in ImageIds)]
 	print ImageIds
 	if len(ImageIds) > 3: # Limit polling to the first 3 images.
 		del ImageIds[3:]
 	for ID in ImageIds:
 		stats = I.Main(ID)
-		head = "\x02[Imgur]\x02 {0} [\x02{1}\x02 views/\x02{2}\x02 bandwidth/\x02{3}\x02]".format(stats['title'],stats['views'],stats['bandwidth'],stats['submitted'])
-		try:
-			tail = " - (\x02{0}\x02 Points)".format(stats['points'])
-			tail += "-(Likes: \x02{1}\x02/\x02{2}\x02%)".format(stats['likes'],stats['likespercent'])
-			tail += "-(Dislikes: \x02{3}\x02/\x02{4}\x02%)".format(stats['dislikes'],stats['dislikespercent'])
-		except:
-			tail = ""
-		img = head+tail
-		sock.send("PRIVMSG {0} :{1}".format(msg[3], img))
-	
+		x = "\x02[Imgur]\x02 {0}".format(stats['title'] if stats['title'] else '')
+		x += " \x02{0}\x02 views".format(stats['views']) if stats['views'] else ''
+		x += " \x02{0}\x02 bandwidth".format(stats['bandwidth']) if stats['bandwidth'] else ''
+		x += " \x02{0}\x02".format(stats['submitted']) if stats['submitted'] else ''
+
+		#Tail end.
+		x += " - \x02{0}\x02 Points".format(stats['points']) if stats['points'] else ''
+		x += " - Likes: \x02{0}\x02/\x02{1}\x02%".format(stats['likes'],stats['likespercent']) \
+			if stats['likes'] and stats['likespercent'] else ''
+		x += " - Dislikes: \x02{0}\x02/\x02{1}\x02%".format(stats['dislikes'],stats['dislikespercent']) \
+			if stats['dislikes'] and stats['dislikespercent'] else ''
+
+		sock.say(msg[3], x
+
 hooks = {
-	'(?:https?:\/\/)?(?:www\.)?(?:i\.)?imgur\.com\/(?:gallery\/)?[a-zA-Z0-9]{5}(?:\.)?(?:jpg|jpeg|png|gif)?': [ImgurStats, 5, False],	
-		}
+	'(?:https?:\/\/)?(?:www\.)?(?:i\.)?imgur\.com\/(?:gallery\/)?[a-zA-Z0-9]{5}(?:\.)?(?:jpg|jpeg|png|gif)?': [ImgurStats, 5, False],
+	}
 
 helpstring = """Parses messages for imgur links, and returns statistics of the image."""
