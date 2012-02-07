0681141e9e525684a58ada20ff1c35edce86f4d8
Ferus <mathewhudson93@gmail.com>
2012-02-06 19:54:47 -0800

    Partial profiles complete, just need CLI parsing now.

--- Core.py
+++ Core.py
@@ -7,9 +7,9 @@ import Parser
 from Services import NickServ, HostServ, Allowed
 
 class Bot():
-	def __init__(self, nickname='', realname='', ident='', owner=[], ssl=True, proxy=None):
+	def __init__(self, nickname='', realname='', ident='', owner=[], ssl=True, proxy=None, logging=False):
 		'''Create our bots name, realname, and ident, and create our IRC object, Commands object, Parser object, and users dict'''
-		self.irc = blackbox.IRC(logging=False, ssl=ssl)
+		self.irc = blackbox.IRC(logging=logging, ssl=ssl)
 		if proxy:
 			try:
 				print("* [IRC] Attempting to set proxy to {0} on port {1}".format(proxy[0], proxy[1]))
@@ -25,22 +25,22 @@ class Bot():
 
 		self.Nickserv = NickServ.NickServ(sock=self.irc)
 		self.Hostserv = HostServ.HostServ(sock=self.irc)
-		
+
 		self.allowed = Allowed.Allowed("Services/AllowedUsers.shelve")
-		
+
 		if owner:
 			self.Owner = owner
 			self.allowed.Owner = self.Owner
 			self.allowed.db[self.Owner[0]] = [self.Owner[1], self.Owner[2]] #Reset the owner. Just in case the config changed.
 			print("* [Access] Setting owner to {0}, with hostmask {1}".format(self.Owner[0], self.Owner[1]))
-		
+
 		self.p = Parser.Parse(sock=self.irc, allowed=self.allowed, nick=nickname)
-				
+
 		self.nickname = nickname if nickname else 'WhergBot2'
 		self.realname = realname if realname else 'WhergBot [Ferus]'
 		self.ident = ident if ident else 'Wherg'
-        
-		
+
+
 	def Connect(self, server, port=6697):
 		'''Connect to the server, default the port to 6697 because SSL'''
 		self.irc.connect(server, port)
@@ -58,11 +58,11 @@ class Bot():
 		self.irc.nickname(self.nickname)
 		print("* [IRC] Sending nickname: {0}".format(self.nickname))
 		self.irc.send("MODE {0} +Bs".format(self.nickname))
-		
+
 	def Identify(self):
 		self.Nickserv.Identify()
 		sleep(.3)
-		
+
 	def Parse(self, msg):
 		try:
 			if not msg:
--- WhergBot.py
+++ WhergBot.py
@@ -1,40 +1,167 @@
 #!/usr/bin/env python
 
 import Core
+
+import os
 from threading import Timer
+from ConfigParser import ConfigParser
+cParser = ConfigParser
 
-nick = 'WhergBot'
-real = 'WhergBot [Ferus]'
-ident = 'Wherg'
-#channels = ['#hacking', '#lobby' ,'#4chon' ,'#circlejerk' , '#games', "#h"]
-channels = ['#h']
-owner = ['Ferus', 'anonymous@the.interwebs', 0]
+def LoadConfig(Profile='WhergBot'):
+	if os.access("./Config.ini", os.R_OK):
+		Config = cParser()
+		Config.read("./Config.ini")
+		print("* [Config] Config file found. Loading config.")
+		if not Config.has_section(Profile):
+			print(Config._sections)
+			import sys
+			sys.exit("* [Config] Config file is missing an important section. Please re-run WhergBot with the -r flag.")
+		try:
+			return Config
+		except:
+			import sys
+			sys.exit("* [Config] Failed to load config. Please re-run WhergBot with the -r flag.")
+	else:
+		print("* [Config] No Config.ini file found. Would you like to create one now?")
+		if raw_input("[Y/N] >> ").lower() not in ('y', 'yes'):
+			import sys
+			sys.exit("You have chosen not to create a config file now. Run WhergBot.py with the -r flag to regenerate a config.")
+		else:
+			return MakeConfig()
 
-server = 'opsimathia.datnode.net'
-#proxy = ("127.0.0.1", 3125, "socks5")
-proxy = None
-ssl = True
-if ssl:
-	port = 6697
-else:
-	port = 6667
+def MakeConfig(AddNewProfile=False):
+	# Shoulda made this one function and return a dict.
+	if not AddNewProfile:
+		print("* [Config] You have chosen to (re-)create the config file.")
+		Prof = cParser()
+		Prof.add_section('WhergBot')
+		print("* [Config] What will the bots' nickname be?")
+		Prof.set('WhergBot', 'Nick', GetAnswer(Prompt="Enter Bots' Name >> ", isStr=True))
+		print("* [Config] What will the bots' realname be?")
+		Prof.set('WhergBot', 'Real', GetAnswer(Prompt="Enter Bots' Realname >> ", isStr=True))
+		print("* [Config] What will the bots' ident be?")
+		Prof.set('WhergBot', 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> ", isStr=True))
+		print("* [Config] What channels will the bot join on start? (Separate with a comma)")
+		Prof.set('WhergBot', 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> ", isStr=True))
+		print("* [Config] What is the name of the bots' Owner? (Likely your nick.)")
+		Prof.set('WhergBot', 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> ", isStr=True))
+		print("* [Config] What is the hostmask of the bots' Owner? (Likely your hostmask.)")
+		Prof.set('WhergBot', 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> ", isStr=True))
+		print("* [Config] What is the default access level of the owner? (If unsure, enter 0)")
+		Prof.set('WhergBot', 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> ", isInt=True))
+		print("* [Config] What server will we be connecting to on start?")
+		Prof.set('WhergBot', 'Server', GetAnswer(Prompt="Enter Server Address >> ", isStr=True))
+		print("* [Config] Will we be using SSL to connect? (If unsure, enter 'False')")
+		Prof.set('WhergBot', 'UseSSL', GetAnswer(Prompt="Enter SSL choice (True/False) >> ", isBool=True))
+		print("* [Config] What port will be used with SSL? (Default 6697)")
+		Prof.set('WhergBot', 'SSLPort', GetAnswer(Prompt="Enter default SSL port >> ", isInt=True))
+		print("* [Config] What port will be used without SSL? (Default 6667)")
+		Prof.set('WhergBot', 'Port', GetAnswer(Prompt="Enter default Non-SSL port >> ", isInt=True))
+		print("* [Config] Will we be logging connections? (Default False)")
+		Prof.set('WhergBot', 'Logging', GetAnswer(Prompt="Enter choice for logging (True/False) >> ", isBool=True))
+		with open("./Config.ini", 'wb') as C:
+			Prof.write(C)
+		print("* [Config] Wrote config file to Config.ini, Please restart WhergBot.")
+		return Prof
+
+	else:
+		Prof = cParser()
+		Prof.read("./Config.ini")
+		print("* [Config] First off, what would you like to name the new profile?")
+		NewConfig = GetAnswer(Prompt="New profile name >> ", isStr=True)
+		print("* [Config] You have chosen to name the profile '{0}'.".format(NewConfig))
+		Prof.add_section(NewConfig)
+		print("* [Config] What will the bots' nickname be?", )
+		Prof.set(NewConfig, 'Nick', GetAnswer(Prompt="Enter Bots' Name >> ", isStr=True))
+		print("* [Config] What will the bots' realname be?")
+		Prof.set(NewConfig, 'Real', GetAnswer(Prompt="Enter Bots' Realname >> ", isStr=True))
+		print("* [Config] What will the bots' ident be?")
+		Prof.set(NewConfig, 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> ", isStr=True))
+		print("* [Config] What channels will the bot join on start? (Separate with a comma)")
+		Prof.set(NewConfig, 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> ", isStr=True))
+		print("* [Config] What is the name of the bots' Owner? (Likely your nick.)")
+		Prof.set(NewConfig, 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> ", isStr=True))
+		print("* [Config] What is the hostmask of the bots' Owner? (Likely your hostmask.)")
+		Prof.set(NewConfig, 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> ", isStr=True))
+		print("* [Config] What is the default access level of the owner? (If unsure, enter 0)")
+		Prof.set(NewConfig, 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> ", isInt=True))
+		print("* [Config] What server will we be connecting to on start?")
+		Prof.set(NewConfig, 'Server', GetAnswer(Prompt="Enter Server Address >> ", isStr=True))
+		print("* [Config] Will we be using SSL to connect? (If unsure, enter 'False')")
+		Prof.set(NewConfig, 'UseSSL', GetAnswer(Prompt="Enter SSL choice (True/False) >> ", isBool=True))
+		print("* [Config] What port will be used with SSL? (Default 6697)")
+		Prof.set(NewConfig, 'SSLPort', GetAnswer(Prompt="Enter default SSL port >> ", isInt=True))
+		print("* [Config] What port will be used without SSL? (Default 6667)")
+		Prof.set(NewConfig, 'Port', GetAnswer(Prompt="Enter default Non-SSL port >> ", isInt=True))
+		print("* [Config] Will we be logging connections? (Default False)")
+		Prof.set(NewConfig, 'Logging', GetAnswer(Prompt="Enter choice for logging (True/False) >> ", isBool=True))
+		with open("./Config.ini", 'wb') as C:
+			Prof.write(C)
+		print("* [Config] New profile has been written, re-run WhergBot with `WhergBot.py -p {0}`".format(NewConfig))
+		return Prof
+
+
+def GetAnswer(Prompt=">> ", isStr=False, isInt=False, isBool=False):
+	try:
+		x = raw_input(Prompt)
+		if not x or len(x) == 0:
+			print("* [Config] You cannot pass a blank argument.")
+			return GetAnswer()
+		if isStr:
+			if type(x) != str:
+				print("* [Config] This setting takes a string type. You entered '{0}' which is a {1} type.".format(x, type(x)))
+				return GetAnswer(isStr=True)
+			else: pass
+
+		if isInt:
+			if not x.isdigit():
+				print("* [Config] This setting takes an int type. You entered '{0}' which is a {1} type.".format(x, type(x)))
+				return GetAnswer(isInt=True)
+			else: x = int(x)
+
+		if isBool:
+			if x.lower() == 'true':
+				return 'True'
+			elif x.lower == 'false':
+				return 'False'
+			else:
+				print("* [Config] This setting takes a bool type. You entered '{0}' which is not a bool.".format(x, type(x)))
+				return GetAnswer(isBool=True)
+		return x
+	except EOFError:
+		print("\n* [Config] That input was not valid.")
+		return GetAnswer(isStr=isStr, isInt=isInt, isBool=isBool)
+
 
 if __name__ == '__main__':
 	try:
-		WhergBot = Core.Bot(nick, real, ident, owner, ssl, proxy=proxy)
-		WhergBot.Connect(server=server, port=port)
+		#Parse CLI options here.
+		Profile='WhergBot'
+		Config = LoadConfig(Profile)
+		WhergBot = Core.Bot(nickname = Config.get(Profile, "nick")
+			,realname = Config.get(Profile, "real")
+			,ident = Config.get(Profile, "ident")
+			,owner = [Config.get(Profile, "ownernick"), Config.get(Profile, "ownerhost"), Config.getint(Profile, "owneraccess")]
+			,ssl = Config.getboolean(Profile, "usessl")
+			,proxy = None
+			,logging = Config.getboolean(Profile, "logging")
+			,)
+
+		WhergBot.Connect(server=Config.get(Profile, "server"), port=Config.getint(Profile, "sslport"))
+		#	Config.getint(Profile, "port") - Non-SSL
+
 		if WhergBot.Nickserv.password != '':
 			_p = Timer(3, WhergBot.Identify, ())
 			_p.daemon = True
 			_p.start()
-		_t = Timer(5, WhergBot.irc.join, (",".join(channels),))
+		_t = Timer(5, WhergBot.irc.join, (Config.get(Profile, "Channels"),))
 		_t.daemon = True
-		_t.start() 
+		_t.start()
 
 		while WhergBot.irc._isConnected:
 			Msg = WhergBot.irc.recv()
 			WhergBot.Parse(Msg)
-			
+
 	except KeyboardInterrupt:
 		print("\n* [Core] Interrupt Caught; Quitting!")
 		WhergBot.p.command.Quit("KeyboardInterrupt Caught!")
