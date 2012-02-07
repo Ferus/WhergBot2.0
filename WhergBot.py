b67dcf1f8b51388eec9c8d88dfbf8310c5922c18
Ferus <mathewhudson93@gmail.com>
2012-02-06 20:16:50 -0800

    making things simple

--- WhergBot.py
+++ WhergBot.py
@@ -35,30 +35,8 @@ def MakeConfig(AddNewProfile=False):
 		print("* [Config] You have chosen to (re-)create the config file.")
 		Prof = cParser()
 		Prof.add_section('WhergBot')
-		print("* [Config] What will the bots' nickname be?")
-		Prof.set('WhergBot', 'Nick', GetAnswer(Prompt="Enter Bots' Name >> ", isStr=True))
-		print("* [Config] What will the bots' realname be?")
-		Prof.set('WhergBot', 'Real', GetAnswer(Prompt="Enter Bots' Realname >> ", isStr=True))
-		print("* [Config] What will the bots' ident be?")
-		Prof.set('WhergBot', 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> ", isStr=True))
-		print("* [Config] What channels will the bot join on start? (Separate with a comma)")
-		Prof.set('WhergBot', 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> ", isStr=True))
-		print("* [Config] What is the name of the bots' Owner? (Likely your nick.)")
-		Prof.set('WhergBot', 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> ", isStr=True))
-		print("* [Config] What is the hostmask of the bots' Owner? (Likely your hostmask.)")
-		Prof.set('WhergBot', 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> ", isStr=True))
-		print("* [Config] What is the default access level of the owner? (If unsure, enter 0)")
-		Prof.set('WhergBot', 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> ", isInt=True))
-		print("* [Config] What server will we be connecting to on start?")
-		Prof.set('WhergBot', 'Server', GetAnswer(Prompt="Enter Server Address >> ", isStr=True))
-		print("* [Config] Will we be using SSL to connect? (If unsure, enter 'False')")
-		Prof.set('WhergBot', 'UseSSL', GetAnswer(Prompt="Enter SSL choice (True/False) >> ", isBool=True))
-		print("* [Config] What port will be used with SSL? (Default 6697)")
-		Prof.set('WhergBot', 'SSLPort', GetAnswer(Prompt="Enter default SSL port >> ", isInt=True))
-		print("* [Config] What port will be used without SSL? (Default 6667)")
-		Prof.set('WhergBot', 'Port', GetAnswer(Prompt="Enter default Non-SSL port >> ", isInt=True))
-		print("* [Config] Will we be logging connections? (Default False)")
-		Prof.set('WhergBot', 'Logging', GetAnswer(Prompt="Enter choice for logging (True/False) >> ", isBool=True))
+		Prof = SetConfig(Prof, 'WhergBot')
+
 		with open("./Config.ini", 'wb') as C:
 			Prof.write(C)
 		print("* [Config] Wrote config file to Config.ini.")
@@ -68,36 +46,45 @@ def MakeConfig(AddNewProfile=False):
 		Prof = cParser()
 		Prof.read("./Config.ini")
 		print("* [Config] First off, what would you like to name the new profile?")
+
 		NewConfig = GetAnswer(Prompt="New profile name >> ", isStr=True)
 		print("* [Config] You have chosen to name the profile '{0}'.".format(NewConfig))
+
 		Prof.add_section(NewConfig)
-		print("* [Config] What will the bots' nickname be?", )
-		Prof.set(NewConfig, 'Nick', GetAnswer(Prompt="Enter Bots' Name >> ", isStr=True))
-		print("* [Config] What will the bots' realname be?")
-		Prof.set(NewConfig, 'Real', GetAnswer(Prompt="Enter Bots' Realname >> ", isStr=True))
-		print("* [Config] What will the bots' ident be?")
-		Prof.set(NewConfig, 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> ", isStr=True))
-		print("* [Config] What channels will the bot join on start? (Separate with a comma)")
-		Prof.set(NewConfig, 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> ", isStr=True))
-		print("* [Config] What is the name of the bots' Owner? (Likely your nick.)")
-		Prof.set(NewConfig, 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> ", isStr=True))
-		print("* [Config] What is the hostmask of the bots' Owner? (Likely your hostmask.)")
-		Prof.set(NewConfig, 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> ", isStr=True))
-		print("* [Config] What is the default access level of the owner? (If unsure, enter 0)")
-		Prof.set(NewConfig, 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> ", isInt=True))
-		print("* [Config] What server will we be connecting to on start?")
-		Prof.set(NewConfig, 'Server', GetAnswer(Prompt="Enter Server Address >> ", isStr=True))
-		print("* [Config] Will we be using SSL to connect? (If unsure, enter 'False')")
-		Prof.set(NewConfig, 'UseSSL', GetAnswer(Prompt="Enter SSL choice (True/False) >> ", isBool=True))
-		print("* [Config] What port will be used with SSL? (Default 6697)")
-		Prof.set(NewConfig, 'SSLPort', GetAnswer(Prompt="Enter default SSL port >> ", isInt=True))
-		print("* [Config] What port will be used without SSL? (Default 6667)")
-		Prof.set(NewConfig, 'Port', GetAnswer(Prompt="Enter default Non-SSL port >> ", isInt=True))
-		print("* [Config] Will we be logging connections? (Default False)")
-		Prof.set(NewConfig, 'Logging', GetAnswer(Prompt="Enter choice for logging (True/False) >> ", isBool=True))
+		Prof = SetConfig(Prof, NewConfig)
+
 		with open("./Config.ini", 'wb') as C:
 			Prof.write(C)
+
 		print("* [Config] New profile has been written, Be sure to specify it when running with `WhergBot.py -p {0}`".format(NewConfig))
+
+		return Prof
+
+def SetConfig(Prof=None, ProfileName=''):
+		print("* [Config] What will the bots' nickname be?")
+		Prof.set(ProfileName, 'Nick', GetAnswer(Prompt="Enter Bots' Name >> ", isStr=True))
+		print("* [Config] What will the bots' realname be?")
+		Prof.set(ProfileName, 'Real', GetAnswer(Prompt="Enter Bots' Realname >> ", isStr=True))
+		print("* [Config] What will the bots' ident be?")
+		Prof.set(ProfileName, 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> ", isStr=True))
+		print("* [Config] What channels will the bot join on start? (Separate with a comma)")
+		Prof.set(ProfileName, 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> ", isStr=True))
+		print("* [Config] What is the name of the bots' Owner? (Likely your nick.)")
+		Prof.set(ProfileName, 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> ", isStr=True))
+		print("* [Config] What is the hostmask of the bots' Owner? (Likely your hostmask.)")
+		Prof.set(ProfileName, 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> ", isStr=True))
+		print("* [Config] What is the default access level of the owner? (If unsure, enter 0)")
+		Prof.set(ProfileName, 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> ", isInt=True))
+		print("* [Config] What server will we be connecting to on start?")
+		Prof.set(ProfileName, 'Server', GetAnswer(Prompt="Enter Server Address >> ", isStr=True))
+		print("* [Config] Will we be using SSL to connect? (If unsure, enter 'False')")
+		Prof.set(ProfileName, 'UseSSL', GetAnswer(Prompt="Enter SSL choice (True/False) >> ", isBool=True))
+		print("* [Config] What port will be used with SSL? (Default 6697)")
+		Prof.set(ProfileName, 'SSLPort', GetAnswer(Prompt="Enter default SSL port >> ", isInt=True))
+		print("* [Config] What port will be used without SSL? (Default 6667)")
+		Prof.set(ProfileName, 'Port', GetAnswer(Prompt="Enter default Non-SSL port >> ", isInt=True))
+		print("* [Config] Will we be logging connections? (Default False)")
+		Prof.set(ProfileName, 'Logging', GetAnswer(Prompt="Enter choice for logging (True/False) >> ", isBool=True))
 		return Prof
 
 
@@ -136,6 +123,9 @@ def GetAnswer(Prompt=">> ", isStr=False, isInt=False, isBool=False):
 if __name__ == '__main__':
 	try:
 		#Parse CLI options here.
+		# -r regenerates new default profile for config
+		# -p specifies specific profile; ommited == 'WhergBot'
+
 		Profile='WhergBot'
 		Config = LoadConfig(Profile)
 		WhergBot = Core.Bot(nickname = Config.get(Profile, "nick")
