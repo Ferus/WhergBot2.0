95269f3ad0bbd1634105626ec5e9415ab3de0388
Ferus <mathewhudson93@gmail.com>
2012-02-10 15:10:36 -0800

    Config option checking, smarter port detection. Defaults for configparser

--- WhergBot.py
+++ WhergBot.py
@@ -5,22 +5,32 @@ import Core
 import os
 from threading import Timer
 from ConfigParser import ConfigParser
-cParser = ConfigParser
+
+CONFIGDEFAULTS = {
+# Feel free to edit these.
+	"nick" : "WhergBot"
+	,"real" : "Wherg"
+	,"ident" : "Wherg"
+	,"ownernick" : "Ferus"
+	,"ownerhost" : "anonymous@the.interwebs"
+	,"owneraccess" : "0"
+	,"usessl" : "False"
+	,"logging" : "False"
+	,"server" : "opsimathia.datnode.net"
+	,"port" : "6667"
+	,"sslport" : "6697"
+	,"channels" : "#hacking"
+	,}
 
 def LoadConfig(Profile='WhergBot'):
-	if os.access("./Config.ini", os.R_OK):
-		Config = cParser()
+	if os.access("./Config.ini", 6):
+		Config = ConfigParser(defaults=CONFIGDEFAULTS)
 		Config.read("./Config.ini")
 		print("* [Config] Config file found. Loading config.")
 		if not Config.has_section(Profile):
-			print(Config._sections)
 			import sys
-			sys.exit("* [Config] Config file is missing an important section. Please re-run WhergBot with the -r flag.")
-		try:
-			return Config
-		except:
-			import sys
-			sys.exit("* [Config] Failed to load config. Please re-run WhergBot with the -r flag.")
+			sys.exit("* [Config] No profile found with the name '{0}'. Please re-run WhergBot with the -r flag to make it.".format(Profile))
+		return Config
 	else:
 		print("* [Config] No Config.ini file found. Would you like to create one now?")
 		if raw_input("[Y/N] >> ").lower() not in ('y', 'yes'):
@@ -30,10 +40,9 @@ def LoadConfig(Profile='WhergBot'):
 			return MakeConfig()
 
 def MakeConfig(AddNewProfile=False):
-	# Shoulda made this one function and return a dict.
 	if not AddNewProfile:
 		print("* [Config] You have chosen to (re-)create the config file.")
-		Prof = cParser()
+		Prof = ConfigParser(defaults=CONFIGDEFAULTS)
 		Prof.add_section('WhergBot')
 		Prof = SetConfig(Prof, 'WhergBot')
 
@@ -43,7 +52,7 @@ def MakeConfig(AddNewProfile=False):
 		return Prof
 
 	else:
-		Prof = cParser()
+		Prof = ConfigParser(defaults=CONFIGDEFAULTS)
 		Prof.read("./Config.ini")
 		print("* [Config] First off, what would you like to name the new profile?")
 
@@ -62,54 +71,60 @@ def MakeConfig(AddNewProfile=False):
 
 def SetConfig(Prof=None, ProfileName=''):
 		print("* [Config] What will the bots' nickname be?")
-		Prof.set(ProfileName, 'Nick', GetAnswer(Prompt="Enter Bots' Name >> ", isStr=True))
+		Prof.set(ProfileName, 'Nick', GetAnswer(Prompt="Enter Bots' Name >> "))
+
 		print("* [Config] What will the bots' realname be?")
-		Prof.set(ProfileName, 'Real', GetAnswer(Prompt="Enter Bots' Realname >> ", isStr=True))
+		Prof.set(ProfileName, 'Real', GetAnswer(Prompt="Enter Bots' Realname >> "))
+
 		print("* [Config] What will the bots' ident be?")
-		Prof.set(ProfileName, 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> ", isStr=True))
+		Prof.set(ProfileName, 'Ident', GetAnswer(Prompt="Enter Bots' Ident >> "))
+
 		print("* [Config] What channels will the bot join on start? (Separate with a comma)")
-		Prof.set(ProfileName, 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> ", isStr=True))
+		Prof.set(ProfileName, 'Channels', GetAnswer(Prompt="Enter Channels (IE: `#lobby,#bots`) >> "))
+
 		print("* [Config] What is the name of the bots' Owner? (Likely your nick.)")
-		Prof.set(ProfileName, 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> ", isStr=True))
+		Prof.set(ProfileName, 'OwnerNick', GetAnswer(Prompt="Enter Owners IRC Nick >> "))
+
 		print("* [Config] What is the hostmask of the bots' Owner? (Likely your hostmask.)")
-		Prof.set(ProfileName, 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> ", isStr=True))
+		Prof.set(ProfileName, 'OwnerHost', GetAnswer(Prompt="Enter Owners Hostmask >> "))
+
 		print("* [Config] What is the default access level of the owner? (If unsure, enter 0)")
-		Prof.set(ProfileName, 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> ", isInt=True))
+		Prof.set(ProfileName, 'OwnerAccess', GetAnswer(Prompt="Enter Owners Access Level >> "))
+
 		print("* [Config] What server will we be connecting to on start?")
-		Prof.set(ProfileName, 'Server', GetAnswer(Prompt="Enter Server Address >> ", isStr=True))
+		Prof.set(ProfileName, 'Server', GetAnswer(Prompt="Enter Server Address >> "))
+
 		print("* [Config] Will we be using SSL to connect? (If unsure, enter 'False')")
 		Prof.set(ProfileName, 'UseSSL', GetAnswer(Prompt="Enter SSL choice (True/False) >> ", isBool=True))
+
 		print("* [Config] What port will be used with SSL? (Default 6697)")
 		Prof.set(ProfileName, 'SSLPort', GetAnswer(Prompt="Enter default SSL port >> ", isInt=True))
+
 		print("* [Config] What port will be used without SSL? (Default 6667)")
 		Prof.set(ProfileName, 'Port', GetAnswer(Prompt="Enter default Non-SSL port >> ", isInt=True))
+
 		print("* [Config] Will we be logging connections? (Default False)")
 		Prof.set(ProfileName, 'Logging', GetAnswer(Prompt="Enter choice for logging (True/False) >> ", isBool=True))
 		return Prof
 
 
-def GetAnswer(Prompt=">> ", isStr=False, isInt=False, isBool=False):
+def GetAnswer(Prompt=">> ", isInt=False, isBool=False):
 	try:
 		x = raw_input(Prompt)
 		if not x or len(x) == 0:
 			print("* [Config] You cannot pass a blank argument.")
 			return GetAnswer()
-		if isStr:
-			if type(x) != str:
-				print("* [Config] This setting takes a string type. You entered '{0}' which is a {1} type.".format(x, type(x)))
-				return GetAnswer(isStr=True)
-			else: pass
 
 		if isInt:
 			if not x.isdigit():
-				print("* [Config] This setting takes an int type. You entered '{0}' which is a {1} type.".format(x, type(x)))
+				print("* [Config] This setting takes an int type. You entered '{0}' which is not an int.".format(x))
 				return GetAnswer(isInt=True)
 			else: x = int(x)
 
 		if isBool:
-			if x.lower() == 'true':
+			if x.lower() in ('true', 't'):
 				return 'True'
-			elif x.lower == 'false':
+			elif x.lower() in ('false', 'f'):
 				return 'False'
 			else:
 				print("* [Config] This setting takes a bool type. You entered '{0}' which is not a bool.".format(x, type(x)))
@@ -135,31 +150,49 @@ if __name__ == '__main__':
 
 	if options.Profile:
 		Profile = options.Profile
+	else:
+		if not options.Newfile:
+			sys.exit("You must specify a profile to use.")
 
 	if options.Newfile:
 		if os.access("./Config.ini", 6):
 			while True:
 				if raw_input("* [Config] Would you like to backup the old config file?\n[Y/N] >> ").lower() in ("y","yes"):
 					os.rename("./Config.ini", "./Config.ini.bak")
-					print("* [Config] backed up the old config file.")
+					print("* [Config] Backed up the old config file.")
 					break
 				else:
+					os.remove("./Config.ini")
 					print("* [Config] We will not backup the old config file.")
 		Profile = 'WhergBot'
 
-	try:
-		Config = LoadConfig(Profile)
-		WhergBot = Core.Bot(
-			nickname = Config.get(Profile, "nick"),
-			realname = Config.get(Profile, "real"),
-			ident = Config.get(Profile, "ident"),
-			owner = [Config.get(Profile, "ownernick"), Config.get(Profile, "ownerhost"), Config.getint(Profile, "owneraccess")],
-			ssl = Config.getboolean(Profile, "usessl"),
-			proxy = None,
-			logging = Config.getboolean(Profile, "logging"))
+	Config = LoadConfig(Profile)
+	if not Config.has_option(Profile, "nick") \
+		or not Config.has_option(Profile, "real") \
+		or not Config.has_option(Profile, "ident") \
+		or not Config.has_option(Profile, "ownernick") \
+		or not Config.has_option(Profile, "ownerhost") \
+		or not Config.has_option(Profile, "owneraccess") \
+		or not Config.has_option(Profile, "usessl") \
+		or not Config.has_option(Profile, "logging") \
+		or not Config.has_option(Profile, "server") \
+		or not Config.has_option(Profile, "port") \
+		or not Config.has_option(Profile, "sslport") \
+		or not Config.has_option(Profile, "channels"):
+			sys.exit("* [Config] Missing option in config file.")
 
-		WhergBot.Connect(server=Config.get(Profile, "server"), port=Config.getint(Profile, "sslport"))
-		#	Config.getint(Profile, "port") - Non-SSL
+	WhergBot = Core.Bot(
+		nickname = Config.get(Profile, "nick"),
+		realname = Config.get(Profile, "real"),
+		ident = Config.get(Profile, "ident"),
+		owner = [Config.get(Profile, "ownernick"), Config.get(Profile, "ownerhost"), Config.getint(Profile, "owneraccess")],
+		ssl = Config.getboolean(Profile, "usessl"),
+		proxy = None,
+		logging = Config.getboolean(Profile, "logging"))
+
+	try:
+		port = Config.getint(Profile, 'port') if not Config.getboolean(Profile, 'usessl') else Config.getint(Profile, 'sslport')
+		WhergBot.Connect(server=Config.get(Profile, "server"), port=port)
 		_t = Timer(5, WhergBot.irc.join, (Config.get(Profile, "Channels"),))
 		_t.daemon = True
 		_t.start()
