1428f5726f2afda966886ec2f15b16d732e62786
Ferus <mathewhudson93@gmail.com>
2012-03-16 20:57:46 -0700

    UD changed their tooltip link to json, fixed regex.

--- Plugins/UrbanDictionary.py
+++ Plugins/UrbanDictionary.py
@@ -2,36 +2,65 @@
 
 import requests
 import os, re
+import json
+
 from htmldecode import convert
 from CommandLock import Locker
 Locker = Locker(5)
 # http://www.urbandictionary.com/tooltip.php?term= <-- Thank god for this url.
 
-def request(word):
+def UDrequest(msg, sock, users, allowed):
+	try:
+		if Locker.Locked:
+			sock.notice(msg[0], "Please wait a little bit longer before using this command")
+			return None
+		word = " ".join(msg[4].split()[1:])
+		if not word:
+			sock.notice(msg[0], "Derp!")
+			return None
+	except Exception, e:
+		print("* [UrbanDict] Error: line 22\n* [UrbanDict] {0}".format(str(e)))
+
 	h = cached(word)
 	if h:
 		print('* [UrbanDict] => Sending cached word.')
-		return "\x02[UrbanDict]\x02 {0}: {1}".format(word, h)
+		sock.say(msg[3], "\x02[UrbanDict]\x02 {0}: {1}".format(word, h))
+		Locker.Lock()
+		return None
 	else:
-		url = "http://www.urbandictionary.com/tooltip.php?term="+word.replace(" ","%20")
+		print("* [UrbanDict] => Polling UrbanDictionary.")
+		url = "http://www.urbandictionary.com/tooltip.php?term="+str(word).replace(" ","%20")
 		try:
-			html = convert(requests.get(url).content)
-		except:
+			html = requests.get(url).content
+		except requests.HTTPError:
 			print("* [UrbanDict] Error => Failed to connect.")
-			return "Failed to connect to Urban Dictionary."
+			sock.say(msg[3], "Failed to connect to Urban Dictionary.")
+			return None
 
-		content = re.compile("<div><b>(.*?)<\/b><\/div><div>(.*?)<\/div>")
-		escapes = re.compile(r'[\r\n\t]')
+		html = html.replace("\\u003C", "<").replace("\\u003E",">")
+		html = json.loads(html)['string']
+
 		try:
-			result = content.match(escapes.sub('', html.replace("<br/>", " ").strip())).groups()[1]
-		except:
+			result = re.sub(r'[\r\n\t]', "", html)
+			result, other = re.search("<div>\s*<b>.*?</b></div><div>\s*(?:.*?<br/><br/>)?(.*?)</div>(?:<div class='others'>\s*(.*?)</div>)?", result).groups()
+		except Exception, e:
+			print("* [UrbanDict] Error: line 47\n* [UrbanDict] {0}".format(repr(e)))
 			result = None
 		if not result or result is None or result == '':
-			return "\x02[UrbanDict]\x02 {0} has not yet been defined.".format(word)
-		else:
-			result = result.replace('&quot;', '"').replace('<b>', '\x02').replace('</b>', '\x02')
-			add_cache(word.lower(), result)
-			return "\x02[UrbanDict]\x02 {0}: {1}".format(word, result)
+			sock.say(msg[3], "\x02[UrbanDict]\x02 {0} has not yet been defined.".format(word))
+			return None
+
+		results = []
+		for x in re.split("<br/>", result):
+			if x == " " or x == "":
+				pass
+			x = x.replace('&quot;', '"').replace('<b>', '\x02').replace('</b>', '\x02').replace('<br/>', '')
+			results.append(x)
+		add_cache(word.lower(), " ".join(results))
+
+		for y in results:
+			sock.say(msg[3], "\x02[UrbanDict]\x02 {0}: {1}".format(word, y))
+		Locker.Lock()
 
 def cached(word):
 	try:
@@ -56,22 +85,8 @@ def add_cache(word, definition=''):
 			print('* [UrbanDict] Cache => Adding word {0}'.format(word))
 			c.write("{0} : {1}\n".format(word, definition))
 
-def UD(msg, sock, users, allowed):
-	try:
-		if not Locker.Locked:
-			x = " ".join(msg[4].split()[1:])
-			if x:
-				sock.say(msg[3], request(x))
-				Locker.Lock()
-			else:
-				sock.notice(msg[0], "Derp!")
-		else:
-			sock.notice(msg[0], "Please wait a little bit longer before using this command")
-	except Exception, e:
-		print("* [UrbanDict] Error:\n* [UrbanDict] {0}".format(str(e)))
-
 hooks = {
-	'^@ud': [UD, 5, False],
+	'^@ud': [UDrequest, 5, False],
 		}
 
 helpstring = "@ud <Word>: Polls UrbanDictionary for a word if that word is not already in the local cache"
