60ee78ed3aacec966196b90d1c6e4eb4a5bf86bc
Ferus <ferus@pyboard.net>
2013-02-10 15:22:19 -0800

    check for ratings somewhat correctly, support youtu.be links, clean some old code

--- Plugins/YouTube/Main.py
+++ Plugins/YouTube/Main.py
@@ -8,7 +8,10 @@ import re
 from Parser import Locker
 Lock = Locker(5)
 
-class YT(object):
+import logging
+logger = logging.getLogger("YouTube")
+
+class YouTube(object):
 	def Main(self, vidId):
 		if not vidId:
 			return None
@@ -16,9 +19,9 @@ class YT(object):
 		try:
 			r.raise_for_status()
 		except (requests.HTTPError, requests.ConnectionError):
-			return None
+			logger.exception("Failed to poll YouTube")
 		r = json.loads(r.text)
-		return self.Parse(r)
+		return self.Parse(r['entry'])
 
 	def ConvertTime(self, seconds):
 		minutes, seconds = divmod(seconds, 60)
@@ -30,71 +33,95 @@ class YT(object):
 		return duration
 
 	def Parse(self, reply):
-		return {'title': reply['entry']['title']['$t']
-			,'duration': self.ConvertTime(int(reply['entry']['media$group']['yt$duration']['seconds']))
-			,'author': reply['entry']['author'][0]['name']['$t']
-			,'averagerating': str(reply['entry']['gd$rating']['average'])[:4]
-			,'maxrating': reply['entry']['gd$rating']['max']
-			,'numLikes': reply['entry']['yt$rating']['numLikes']
-			,'numDislikes': reply['entry']['yt$rating']['numDislikes']
-			,'viewCount': reply['entry']['yt$statistics']['viewCount']}
+		h = {}
+		h['author'] = reply['author'][0]['name']['$t']
+		h['title'] = reply['title']['$t']
+		h['viewCount'] = int(reply['yt$statistics']['viewCount'])
+		h['link'] = "https://youtu.be/"+reply['id']['$t'].split(":")[-1]
+		h['duration'] = self.ConvertTime(int(reply['media$group']['yt$duration']['seconds']))
+
+		if reply.get('yt$rating'):
+			h['averagerating'] = reply['yt$rating'].get('average', None)
+			if h['averagerating'] != None:
+				h['averagerating'] = "{:.2}".format(h['averagerating'])
+			h['maxrating'] = reply['yt$rating'].get('max', None)
+			h['numLikes'] = int(reply['yt$rating'].get('numLikes', None))
+			h['numDislikes'] = int(reply['yt$rating'].get('numDislikes', None))
+
+		elif reply.get('gd$rating'):
+			h['averagerating'] = reply['gd$rating'].get('average', None)
+			if h['averagerating'] != None:
+				h['averagerating'] = "{:.2}".format(h['averagerating'])
+			h['maxrating'] = reply['gd$rating'].get('max', None)
+			h['numLikes'] = int(reply['gd$rating'].get('numLikes', None))
+			h['numDislikes'] = int(reply['gd$rating'].get('numDislikes', None))
+
+		else:
+			h['averagerating'] = None
+			h['maxrating'] = None
+			h['numRatings'] = None
+			h['numLikes'] = None
+			h['numDislikes'] = None
+		return h
 
 	def Search(self, terms, results=2):
 		link = "https://gdata.youtube.com/feeds/api/videos?q={0}&safeSearch=none&max-results={1}&v=2&alt=json".format(terms, results)
 		try:
 			r = requests.get(link)
 			if r.status_code != 200:
-				return None #Error'd Wat.
+				return None
 			r = json.loads(r.text)['feed']['entry']
 		except Exception as e:
-			print("* [YouTube] Error:\n* [YouTube] {0}".format(repr(e)))
+			logger.exception("Failed to get search info")
 			return ['\x02[YouTube]\x02 Failed to get info.']
 
 		results = []
 		for vidInfo in r:
-			Uploader = vidInfo['author'][0]['name']['$t']
-			VidTitle = vidInfo['title']['$t']
-			VidAverage = "{:.2}".format(vidInfo['gd$rating']['average']) # Breaks Py2.6
-			VidMax = vidInfo['gd$rating']['max']
-			VidNumRaters = vidInfo['gd$rating']['numRaters']
-			VidViewCounts = vidInfo['yt$statistics']['viewCount']
-			VidFavoriteCount = vidInfo['yt$statistics']['favoriteCount']
-			VidLink = "https://youtu.be/"+vidInfo['id']['$t'].split(":")[-1]
-			VidLikes = vidInfo['yt$rating']['numLikes']
-			VidDisLikes = vidInfo['yt$rating']['numDislikes']
-			VidLength = self.ConvertTime(int(vidInfo['media$group']['yt$duration']['seconds']))
-
+			vidInfo = self.Parse(vidInfo)
 			# <Title + Uploader> [Length] <Link> [Average/Max Likes/Dislikes] <Views>
-			info = "\x02[YouTube]\x02 Title: \x02{0}\x02".format(VidTitle)
-			info += "- By: \x02{0}\x02".format(Uploader)
-			info += " [Length: \x02{0}\x02]".format(VidLength)
-			info += " <Link: {0}>".format(VidLink)
-			info += " [Rating: \x02{0}\x02/\x02{1}\x02]".format(VidAverage, VidMax)
-			info += " <Likes/Dislikes: \x02{0}\x02/\x02{1}\x02>".format(VidLikes, VidDisLikes)
-			info += " [Views: \x02{0}\x02]".format(VidViewCounts)
+			info = "\x02[YouTube]\x02 {0}".format(vidInfo['title'])
+			info += " - By: \x02{0}\x02".format(vidInfo['author'])
+			info += " - [\x02{0}\x02]".format(vidInfo['duration'])
+			if (vidInfo['averagerating'] != None and vidInfo['maxrating'] != None):
+				info += " [Rating: \x02{0}\x02/\x02{1}\x02]".format(vidInfo['averagerating'], vidInfo['maxrating'])
+			if (vidInfo['numLikes'] != None and vidInfo['numDislikes'] != None):
+				info += " - \x02{:,}\x02 Likes".format(vidInfo['numLikes'])
+				info += " - \x02{:,}\x02 Dislikes".format(vidInfo['numDislikes'])
+			info += " - \x02{:,}\x02 Views".format(vidInfo['viewCount'])
+			info += " - {0}".format(vidInfo['link'])
 			results.append(info)
 		return results
 
 
-class Main(YT):
+class Main(object):
 	def __init__(self, Name, Parser):
 		self.__name__ = Name
 		self.Parser = Parser
 		self.IRC = self.Parser.IRC
-		YT.__init__(self)
+		self.YT = YouTube()
 
-	def YouTubeStats(self, data):
-		vidIds = list(set(re.findall("v=([a-zA-Z0-9_\-]{11})", ' '.join(data[3:]) )))
+	def YouTubeLink(self, data):
+		vidIds = list(set(re.findall("v=([\w\d_\-]{11})", ' '.join(data[3:]) )))
 		for vidId in vidIds:
-			x = self.Main(vidId)
+			self.YouTubeStats(data, vidId)
+
+	def YouTubeShortLink(self, data):
+		vidIds = list(set(re.findall("/([\w\d_\-]{11})", " ".join(data[3:]) )))
+		for vidId in vidIds:
+			self.YouTubeStats(data, vidId)
+
+	def YouTubeStats(self, data, vidId):
+			x = self.YT.Main(vidId)
 			if x != None:
 				info = "\x02[YouTube]\x02 {0}".format(x['title'])
 				info += " - By: \x02{0}\x02".format(x['author'])
 				info += " [\x02{0}\x02]".format(x['duration'])
-				info += " - \x02{0}\x02/\x02{1}\x02".format(x['averagerating'], x['maxrating'])
-				info += " - \x02{0}\x02 Views".format(x['viewCount'])
-				info += " - \x02{0}\x02 Likes".format(x['numLikes'])
-				info += " - \x02{0}\x02 Dislikes".format(x['numDislikes'])
+				if (x['averagerating'] != None and x['maxrating'] != None):
+					info += " - \x02{0}\x02/\x02{1}\x02".format(x['averagerating'], x['maxrating'])
+				if (x['numLikes'] != None and x['numDislikes'] != None):
+					info += " - \x02{:,}\x02 Likes".format(x['numLikes'])
+					info += " - \x02{:,}\x02 Dislikes".format(x['numDislikes'])
+				info += " - \x02{:,}\x02 Views".format(x['viewCount'])
 				self.IRC.say(data[2], info)
 			else:
 				pass
@@ -107,13 +134,14 @@ class Main(YT):
 			terms = "+".join(data[4:]).replace("|","%7C")
 		except Exception:
 			self.IRC.say(data[2], "Supply some searchterms you derp!")
-		for vid in self.Search(terms, results=3):
+		for vid in self.YT.Search(terms, results=3):
 			self.IRC.say(data[2], vid)
 		Lock.Lock()
 
 	def Load(self):
 		self.Parser.hookCommand("PRIVMSG", self.__name__
-			,{"(?:https?:\/\/)?(?:www\.)?youtube\.com\/.*?": self.YouTubeStats
+			,{"(?:https?:\/\/)?(?:www\.)?youtube\.com\/.*?": self.YouTubeLink
+			,"(?:https?:\/\/)?(?:www\.)?youtu.be/([\w\d_\-]{11})": self.YouTubeShortLink
 			,"^@yt .*?$": self.YouTubeGetVids}
 		)
 
